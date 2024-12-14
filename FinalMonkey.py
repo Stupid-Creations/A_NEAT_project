be71220b97299ec0abbc115356fcfbf0f27ad981
Raghav <97241429+Stupid-Creations@users.noreply.github.com>
2024-12-14 09:27:15 +0530

    Add files via upload
    Added Reproduction, Made some small changes such as adding reproduction

--- FinalMonkey.py
+++ FinalMonkey.py
@@ -3,20 +3,13 @@ import math
 
 sigmoid = lambda x: 1 / (1 + math.exp(-x))
 
-def ConnIn(object,list):
-    for i in list:
-        if object == i:
-            return True
-    return False
-
 class Node:
-    def __init__(self, index, connections=None, type="H"):
-        if connections is None:
-            connections = []
+    def __init__(self, index, connections=[], type="H",parent=None):
         self.type = type
         self.index = index
         self.connections = connections
         self.pred = None
+        self.parent = parent
 
     def activate(self, input_val=None):
         if input_val is not None:
@@ -32,7 +25,6 @@ class Connection:
         self.innov = self.get_innov(innov)
         self.disabled = disabled
         self.pred = None
-        #print(f"Weight: {self.weight}, Input Index: {self.input.index}, Output Index: {self.output.index}")
 
     def get_innov(self, innov):
         for conn in innov:
@@ -49,8 +41,9 @@ class Connection:
 class Network:
     def __init__(self, input_size, output_size, innov):
         self.pred = []
-        self.info = [input_size, output_size]
-        self.Nodes = [[Node(i) for i in range(input_size)], [Node(input_size + j) for j in range(output_size)], []]
+        adjusted_input = input_size+1
+        self.info = [adjusted_input, output_size]
+        self.Nodes = [[Node(i) for i in range(self.info[0])], [Node(self.info[0] + j) for j in range(output_size)], []]
         self.Connections = []
         self.fitness = None
 
@@ -60,14 +53,15 @@ class Network:
                 out_node.connections.append(connection)
                 self.Connections.append(connection)
 
-        self.FNodes = [node for sublist in self.Nodes for node in sublist]
-
     def activate(self, input_vals):
-        if len(input_vals) != self.info[0]:
+        if len(input_vals) != self.info[0]-1:
             raise ValueError("Incorrect Input Size")
         
         for i, node in enumerate(self.Nodes[0]):
-            node.activate(input_val=input_vals[i])
+            try:
+                node.activate(input_val=input_vals[i])
+            except:
+                node.activate(input_val=1)
 
         for node in self.Nodes[2]:
             node.activate()
@@ -93,6 +87,8 @@ class Network:
         self.add_connection(doomed.input, new_node, innov,weight = 1)
         self.add_connection(new_node, doomed.output, innov,weight = doomed.weight)
 
+    def incheck(self,list):
+        return self in list
     def mutate_weight(self):
         for i in self.Nodes[1]:
             for j in i.connections:
@@ -128,7 +124,50 @@ class Network:
                 else:
                     print("dense net found, redoing")
                     self.mutate(innov,mut=random.choice([0,1]))
-                    
+
+def NodeIndexSquared(List,Index):
+    for i in range(len(List)):
+        if List[i].index == Index:
+            return i
+    raise ValueError("Not in List")
+
+def reproduce(pa,pb,innov):
+    fitter = pa if pa.fitness > pb.fitness else pb
+    other = pa if fitter == pb else pb
+
+    pfc = sorted(fitter.Connections,key=lambda x: x.innov)
+    poc = sorted(other.Connections,key=lambda x: x.innov)
+
+    disjoint = [i for i in pfc if i not in poc]
+    common = [random.choice([x,y]) for x,y in zip(pfc,poc) if x.innov==y.innov]
+
+    final = disjoint+common
+    allnodes = list(set([i.output.index for i in final]+[j.input.index for j in final]))
+
+    Nodes = [Node(i,connections=[]) for i in sorted(allnodes)]
+    Connections = []
+    for i in range(len(final)):
+        for j in range(len(Nodes)):
+            if Nodes[j].index == final[i].output.index:
+                connection = Connection(Nodes[j],innov,Nodes[NodeIndexSquared(Nodes,final[i].input.index)],disabled = final[i].disabled,weight=final[i].weight)
+                Nodes[j].connections.append(connection)
+                Connections.append(connection)
+    
+    DivNodes = [[],[],[]]
+    for i in range(pa.info[0]):
+        DivNodes[0].append(Nodes[i])
+    for i in range(pa.info[0],pa.info[1]+pa.info[0]):
+        DivNodes[1].append(Nodes[i])
+    for i in range(pa.info[1]+pa.info[0],len(Nodes)):
+        DivNodes[2].append(Nodes[i])
+    
+    child = Network(pa.info[0]-1,pa.info[1],innov)
+    child.Connections = Connections
+    child.Nodes = DivNodes
+
+    child.mutate(innov)
+
+    return child            
         
 def print_connections(network):
     for connection in network.Connections:
@@ -141,11 +180,18 @@ def print_connections(network):
 
 
 inn = []
-a = Network(3,2, inn)
-a.activate([1,2,3])
-print("Output Predictions:", a.pred)
-a.add_node(inn)
-a.activate([1,2,3])
-print("Output Predictions:", a.pred)
 
-print_connections(a)
+a = Network(3,3,inn)
+a.add_node(inn)
+
+b = Network(3,3,inn)
+
+a.fitness,b.fitness = 1,-1
+c = reproduce(a,b,inn)
+print_connections(c)
+c.activate([1,3,4])
+print(c.pred)
+
+#TODO: ADD FUNCTION TO CHECK FOR DENSE NETS (or just use try catch lol)
+#TODO 2 Electric Bogaloo: ADD RECURSIVE NETWORK FUNCTIONALITY TO ACTIVATION AND MUTATION
+#TODO 3: Add Bias Node
